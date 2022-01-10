7e04c249324caf5b0ddbb49392ed3d05d4daba79
jonah tjandra <jonahtjandra@gmail.com>
2022-01-10 16:43:18 +0700

    added virtual environment

---
+++ src/email_filler.py
@@ -0,0 +1,5 @@
+class EmailFiller:
+    
+    # template : string representation of relative path of a certain 
+    def __init__(self, template : str, csv : str) -> None:
+        print("Hello, World!")
